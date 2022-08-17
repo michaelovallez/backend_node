f11e1466294c975c877c1a680eda19c1fabba615
Mike Ovallez <michaelovallez@policia.sanjuan.gob.ar>
2022-08-16 23:14:57 -0300

    database

--- index.js
+++ index.js
@@ -9,7 +9,10 @@ if (process.env.PORT) {
 }
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb+srv://michaelovallez:eidfs2008MAMA**@cluster0.0yidav5.mongodb.net/?retryWrites=true&w=majority')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/portafolio',{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
     .then(()=> {
         console.log("conexion a db establecida correctamente");
         //creacion del servidor
